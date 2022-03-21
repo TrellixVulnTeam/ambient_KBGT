0f55085e7b04d11172b826e2d6b341759eb14e49
yyygggg22 <nanyi3@illinois.edu>
2022-03-20 23:42:59 -0500

    Update show_test.py
    Finish the show_test.py

--- show_test.py
+++ show_test.py
@@ -21,17 +21,21 @@ user_input = main()
 chord_progression = user_input[0]
 bass_line = user_input[1]
 
-# Convert into Music21 chord format:
+# Remove the bass line from chord progression:
+temp_chord = []
+bass_line_removed = []
 for i in range(len(chord_progression)):
-    part_1.append(chord.Chord(chord_progression[i], type='whole'))
+    temp_chord = chord_progression[i]
+    temp_chord.pop(0)
+    bass_line_removed.append(temp_chord)
+
+# Convert into Music21 chord format:
+for j in range(len(chord_progression)):
+    part_1.append(chord.Chord(bass_line_removed[j], type='whole'))
 
 # Convert into Music21 note format:
-for j in range(len(bass_line)):
-    part_2.append(note.Note(bass_line[j], type='whole'))
-
-# chord1 = chord.Chord(['c3', 'g4', 'c5', 'e5'], type='whole')
-# chord2 = chord.Chord(['f3', 'bb4', 'c5', 'f5'], type='whole')
-# chord3 = chord.Chord(['g3', 'bb4', 'd5', 'f5'], type='whole')
+for k in range(len(bass_line)):
+    part_2.append(note.Note(bass_line[k], type='whole'))
 
 score.insert(0, part_1)
 score.insert(0, part_2)
